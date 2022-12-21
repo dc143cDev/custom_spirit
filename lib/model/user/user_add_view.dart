db55e05ab250516bd02a151687c61d3dfcd87cfb
dc143cDev <devc143c@gmail.com>
2022-12-21 22:33:44 +0900

    db 연동 데이터 ui 출력 성공

---
+++ lib/model/user/user_add_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserAddView extends StatefulWidget {
+  const UserAddView({Key? key}) : super(key: key);
+
+  @override
+  State<UserAddView> createState() => _UserAddViewState();
+}
+
+class _UserAddViewState extends State<UserAddView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
